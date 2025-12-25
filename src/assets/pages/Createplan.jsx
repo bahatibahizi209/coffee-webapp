5e881f785b0c2d1d67af805fc43ef6c4cb4ce22f
bahatibahizi209 <bahatibahizi209@gmail.com>
2025-12-25 12:15:38 +0200

    add assets and components

--- src/assets/pages/Createplan.jsx
+++ src/assets/pages/Createplan.jsx
@@ -18,7 +18,30 @@ function Createplan() {
             </div>
             </section>
             {/* step section */}
-            
+            <section className="max-w-7xl max-auto px-6 py-16">
+            <div className="bg-gray-800 rounded-lg px-6 sm:px-16 sm:py-20 mb-16">
+            <div className="relative">
+            <div className="absolute top-6 left-6 right-6 h-0.5 bg-orange-200 hidden sm:block"/>
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 relative">
+          {/* use map method */}
+          <div className="text-center sm:text-left">
+          <div className="w-12 h-12 rounded-full border-2
+          border-teal-600 bg-gray-800 mb-6 mx-auto sm:mx-0"/>
+          <h3 className="text-5xl sm:text-7xl font-bold text-orange-200 mb-4">01</h3>
+          <h4 className="text-xl sm:text-2xl font-bold text-white mb-3">Title</h4>
+          <p className="text-white opacity-80 leading-relaxed text-sm sm:text-base">Description</p>       
+          </div>
+          </div>
+            </div>
+            </div>
+            </section>
+          {/*   Accordion + sidebar */}
+          <section className="max-w-7xl mx-auto px-6 pb-32">
+          <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 lg:gap-16">
+          {/* sidebar */}
+          <div className=""></div>
+          </div>
+          </section>
     </div>
   )
 }
